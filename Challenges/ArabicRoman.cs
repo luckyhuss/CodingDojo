785dd7ed9cffe205ef4a0b228bc88c54f1769c15
Anwar Buchoo <luckyhuss@msn.com>
2016-11-10 11:47:44 +0400

    Arabic to Roman challenge

---
+++ Challenges/ArabicRoman.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo.Challenges
+{
+    /// <summary>
+    /// First Coding DOJO
+    /// 03/11/16 ; 10/11/16
+    /// </summary>
+    class ArabicRoman
+    {
+        /// <summary>
+        /// Convert Arabic to Roman numbers
+        /// </summary>
+        /// <param name="number">Arabic number</param>
+        /// <returns>Roman number</returns>
+        public static string ArabicToRoman(int number)
+        {
+            if ((number < 0) || (number > 4999)) throw new ArgumentOutOfRangeException("Value should be b/w 1 and 4999");
+
+            if (number >= 1000) return String.Format("M{0}", ArabicToRoman(number - 1000));
+            if (number >= 900) return String.Format("CM{0}", ArabicToRoman(number - 900));
+            if (number >= 500) return String.Format("D{0}", ArabicToRoman(number - 500));
+            if (number >= 400) return String.Format("CD{0}", ArabicToRoman(number - 400));
+            if (number >= 100) return String.Format("C{0}", ArabicToRoman(number - 100));
+            if (number >= 90) return String.Format("XC{0}", ArabicToRoman(number - 90));
+            if (number >= 50) return String.Format("L{0}", ArabicToRoman(number - 50));
+            if (number >= 40) return String.Format("XL{0}", ArabicToRoman(number - 40));
+            if (number >= 10) return String.Format("X{0}", ArabicToRoman(number - 10));
+            if (number >= 9) return String.Format("IX{0}", ArabicToRoman(number - 9));
+            if (number >= 5) return String.Format("V{0}", ArabicToRoman(number - 5));
+            if (number >= 4) return String.Format("IV{0}", ArabicToRoman(number - 4));
+            if (number >= 1) return String.Format("I{0}", ArabicToRoman(number - 1));
+            if (number == 0) return string.Empty;
+
+            throw new ArgumentOutOfRangeException("Please check your input value");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CodingDojo.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Format("{0} -> {1}", 4888, ArabicRoman.ArabicToRoman(4888)));
+            Console.WriteLine(String.Format("{0} -> {1}", 1515, ArabicRoman.ArabicToRoman(1515)));
+            Console.WriteLine(String.Format("{0} -> {1}", 2002, ArabicRoman.ArabicToRoman(2002)));
+            Console.WriteLine(String.Format("{0} -> {1}", 1030, ArabicRoman.ArabicToRoman(1030)));
+            Console.WriteLine(String.Format("{0} -> {1}", 900, ArabicRoman.ArabicToRoman(900)));
+            Console.WriteLine(String.Format("{0} -> {1}", 666, ArabicRoman.ArabicToRoman(666)));
+            Console.WriteLine(String.Format("{0} -> {1}", 509, ArabicRoman.ArabicToRoman(509)));
+            Console.WriteLine(String.Format("{0} -> {1}", 15, ArabicRoman.ArabicToRoman(15)));
+            Console.WriteLine(String.Format("{0} -> {1}", 14, ArabicRoman.ArabicToRoman(14)));
+            Console.WriteLine(String.Format("{0} -> {1}", 13, ArabicRoman.ArabicToRoman(13)));
+            Console.WriteLine(String.Format("{0} -> {1}", 12, ArabicRoman.ArabicToRoman(12)));
+            Console.WriteLine(String.Format("{0} -> {1}", 11, ArabicRoman.ArabicToRoman(11)));
+
+            Console.WriteLine();
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(String.Format("{0} -> {1}", i, ArabicRoman.ArabicToRoman(i)));
+            }           
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
